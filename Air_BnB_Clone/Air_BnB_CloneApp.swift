d85519a7b68dd8b52ef9a8d71c8aadc8f3f31090
Dev Patel <dpatel222430@gmail.com>
2024-08-08 21:20:07 +0530

    Initial Commit

---
+++ Air_BnB_Clone/Air_BnB_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_BnB_CloneApp.swift
+//  Air_BnB_Clone
+//
+//  Created by Dev Patel on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Air_BnB_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
